64bfc9182c79b44962176b14a94c9ed982bce52a
dreamclinger <chenhaomails@gmail.com>
2014-12-05 16:05:28 +0800

    publish version 1.0

---
+++ test.mm
@@ -0,0 +1,14 @@
+<map version="0.8.0">
+  <node CREATED="1417667122098" MODIFIED="1417667122098" TEXT="Root node">
+    <node CREATED="1417667122098" MODIFIED="1417667122098" TEXT="First level">
+      <node CREATED="1417667122098" MODIFIED="1417667122098" TEXT="Subnode">
+        <node CREATED="1417667122098" MODIFIED="1417667122098" TEXT="Subsubnode">
+          <node CREATED="1417667122098" MODIFIED="1417667122098" TEXT="Subsubsubnode" />
+        </node>
+      </node><node CREATED="1417667122098" MODIFIED="1417667122098" TEXT="Subnode" />
+    </node><node CREATED="1417667122098" MODIFIED="1417667122098" TEXT="Another first level">
+      <node CREATED="1417667122098" MODIFIED="1417667122098" TEXT="Subnode with some longer text" />
+      <node CREATED="1417667122098" MODIFIED="1417667122098" TEXT="Another subnode with even longer text,&#10;but fortunately it can be wrapped onto&#10;multiple lines." />
+    </node>
+  </node>
+</map>

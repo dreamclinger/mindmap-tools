39cc44c3280ffdd9631dc25b07d3391259bba0c0
dreamclinger <chenhaomails@gmail.com>
2015-04-21 13:06:28 +0800

    update

--- example.mm
+++ example.mm
@@ -1,25 +1,25 @@
 <map version="0.8.0">
-  <node CREATED="1427647010291" MODIFIED="1427647010291" TEXT=" Transparent_OS_Setting">
-    <node CREATED="1427647010291" MODIFIED="1427647010291" TEXT="General">
-      <node CREATED="1427647010291" MODIFIED="1427647010291" TEXT="Idea: MintLinux">
-        <node CREATED="1427647010291" IMAGE="./source/mintlinux.jpg" >
-<richcontent TYPE="NODE"><html><head></head><body><img src="./source/mintlinux.jpg"/></body></html></richcontent></node>
-      </node><node CREATED="1427647010291" MODIFIED="1427647010291" TEXT="Object: Common apps" />
-      <node CREATED="1427647010291" MODIFIED="1427647010291" TEXT="Need: (OR)">
-        <node CREATED="1427647010291" MODIFIED="1427647010291" TEXT="Theme" />
-        <node CREATED="1427647010291" MODIFIED="1427647010291" TEXT="Extension" />
-        <node CREATED="1427647010291" MODIFIED="1427647010291" TEXT="Setting" />
-      </node><node CREATED="1427647010291" MODIFIED="1427647010291" TEXT="Example:">
-        <node CREATED="1427647010291" MODIFIED="1427647010291" TEXT="Console" />
-        <node CREATED="1427647010291" MODIFIED="1427647010291" TEXT="WebBrowser" />
-        <node CREATED="1427647010291" MODIFIED="1427647010291" TEXT="Example-Pdf">
-          <node CREATED="1427647010291" LINK="./source/Chapter01_examples.pdf" />
+  <node CREATED="1427879697790" MODIFIED="1427879697790" TEXT=" Transparent_OS_Setting">
+    <node CREATED="1427879697790" MODIFIED="1427879697790" TEXT="General">
+      <node CREATED="1427879697790" MODIFIED="1427879697790" TEXT="Idea: MintLinux">
+        <node CREATED="1427879697790" IMAGE="./source/mintlinux.jpg" >
+<richcontent TYPE="NODE"><html><head/><body><img src="./source/mintlinux.jpg"/></body></html></richcontent></node>
+      </node><node CREATED="1427879697790" MODIFIED="1427879697790" TEXT="Object: Common apps" />
+      <node CREATED="1427879697790" MODIFIED="1427879697790" TEXT="Need: (OR)">
+        <node CREATED="1427879697790" MODIFIED="1427879697790" TEXT="Theme" />
+        <node CREATED="1427879697790" MODIFIED="1427879697790" TEXT="Extension" />
+        <node CREATED="1427879697790" MODIFIED="1427879697790" TEXT="Setting" />
+      </node><node CREATED="1427879697790" MODIFIED="1427879697790" TEXT="Example:">
+        <node CREATED="1427879697790" MODIFIED="1427879697790" TEXT="Console" />
+        <node CREATED="1427879697790" MODIFIED="1427879697790" TEXT="WebBrowser" />
+        <node CREATED="1427879697790" MODIFIED="1427879697790" TEXT="Example-Pdf">
+          <node CREATED="1427879697790" LINK="./source/Chapter01_examples.pdf" />
         </node>
       </node>
-    </node><node CREATED="1427647010291" MODIFIED="1427647010291" TEXT="Suitable_Apps">
-      <node CREATED="1427647010291" MODIFIED="1427647010291" TEXT="Item2">
-        <node CREATED="1427647010291" LINK="http://github.com/dreamclinger/solarized/" />
-      </node><node CREATED="1427647010291" MODIFIED="1427647010291" TEXT="Chrome" />
+    </node><node CREATED="1427879697790" MODIFIED="1427879697790" TEXT="Suitable_Apps">
+      <node CREATED="1427879697790" MODIFIED="1427879697790" TEXT="Item2">
+        <node CREATED="1427879697790" LINK="http://github.com/dreamclinger/solarized/" />
+      </node><node CREATED="1427879697790" MODIFIED="1427879697790" TEXT="Chrome" />
     </node>
   </node>
 </map>
--- text-to-freemind-image.py
+++ text-to-freemind-image.py
@@ -8,17 +8,28 @@ lines=f.readlines()
 fn=open(nfilename, "w")
 
 pattern = 'IMAGE='
-pre_text='<richcontent TYPE="NODE"><html><head></head><body><img src='
-pos_text='/></body></html></richcontent></node>'
+end_node = '/>'
+#pre_text1='<richcontent TYPE="NODE"><html><head></head><body><img src='
+pre_text1='<richcontent TYPE="NODE"><html><head/><body><img src='
+pos_text1='/></body></html></richcontent></node>'
+
+pre_text2='<richcontent TYPE="NODE"><html><head/><body><img src='
+pos_text2='/></body></html></richcontent>'
 
 for i in range(0,len(lines)):
 	line=lines[i]
 	if((line.find(pattern)!=-1)):
 		text = line.split(pattern)[1].strip()
+		text = text.replace(">", "")
+		text = text.replace("<", "")
 		src = re.split(r"\s",text)[0]
-		line = line.replace("/>", ">")
+		if((line.find(end_node)!=-1)):
+			line = line.replace("/>", ">")
+			new_line = pre_text1 + src + pos_text1
+		else:
+			new_line = pre_text2 + src + pos_text2
 		fn.write(line)
-		fn.write(pre_text + src + pos_text +'\n')
+		fn.write(new_line + '\n')
 	else:
 		fn.write(line)
 cmd1="mv " + nfilename + " " + filename
